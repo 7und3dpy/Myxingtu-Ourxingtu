669a4c06a2674e94638a30213a78724e3200db16
Tran Minh Tuan <tuan.tm214978@sis.hust.edu.vn>
2024-02-22 09:46:24 +0700

    up

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { useState } from "react";
+
+export default function Page() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "Không nhé",
+      "Khó",
+      "Mơ đi nha cưng",
+      "Nhưng e chưa biết nhiều về anh",
+      "Em cũng chưa sẵn sàng cho sự thật này",
+      "Mong chúng ta là bạn",
+      "Em còn nhiều việc quan trọng khác",
+      ":((((",
+      "No :(",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  return (
+    <div className="-mt-16 flex h-screen flex-col items-center justify-center">
+      {yesPressed ? (
+        <>
+          <img src="https://gifdb.com/images/high/bear-hug-couple-milk-mocha-5rh2rx1rk525w5cs.gif" />
+          <div className="my-4 text-4xl font-bold">Ứ, em chờ câu này lây lắm rồi :3</div>
+        </>
+      ) : (
+        <>
+          <img
+            className="h-[200px]"
+            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+          />
+          <h1 className="my-4 text-4xl">Liệu anh có thể theo đuổi em?</h1>
+          <div className="flex items-center">
+            <button
+              className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              onClick={handleNoClick}
+              className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
